6a8123d3b5766f712c9e645a04b528cdfb97d6b5
abhis324 <abhishekmaphy98@gmail.com>
2019-01-09 17:17:29 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express')
+ 
+var app = express()
+ 
+app.get('/amanchutiya', function(req, res) {
+  res.json({notes: "Hi aman....do u have any randi contact number??"})
+})
+ app.get('/katti', (req,res) => {
+ 	res.send("tatti... aman tu to muh m le")
+ })
+app.listen(3000, () => {"Your app is listening on port 3000!!!"})                          x

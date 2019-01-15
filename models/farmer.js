da1f914a040e3710c8298e6205b11098d3d4b963
abhis324 <abhishekmaphy98@gmail.com>
2019-01-15 10:00:38 +0530

    my commit initial one

---
+++ models/farmer.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// schema banega neeche
+
+// const BookSchema = new Schema({
+// 	title: String,
+// 	pages: Number,
+// })
+
+const FarmerSchema = new Schema({
+
+	name: String,
+	aadhar: Number,
+	state: String,
+	district: String,
+	password: String,
+	pincode: Number,
+
+})
+
+// const AuthorSchema = new Schema({
+// 	name: String,
+// 	age: Number,
+// 	books: [BookSchema]
+// })
+
+//ab model bnega
+
+const Farmer = mongoose.model('farmer',FarmerSchema)
+
+FarmerSchema.methods.validPassword = function(user,password){
+
+	Farmer.findOne({name : user}).then(function(result){
+
+		return (result.password === password)
+
+	})
+
+}
+
+module.exports = Farmer;
---
+++ test/save_test.js
@@ -0,0 +1,39 @@
+const mocha = require('mocha')
+
+const assert = require('assert')
+
+const Farmer = require('../models/farmer')
+
+
+describe('Saving records of Farmers', function(){
+
+	it ('saves a farmer record to the database',function(done){
+
+
+		assert('aman' + 'chutiya' === 'amanchutiya')
+		done()
+
+		// var char = new MarioChar({
+		// 	name: 'Kutta',
+		// 	weight: 15
+		// })
+
+		// char.save().then(function(){
+		// 	assert(char.isNew === false);
+		// 	done();
+		// })
+
+			// Farmer.findOne({district:'patna'}).then(function(result){
+
+			// 	// console.log(result)
+
+			// 	assert(result.district === 'patna')
+
+			// 	done()
+			// })
+
+	})
+
+	// next tests
+
+})
